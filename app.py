2d8d03921502a4daad13bf128339fb168e71b0b6
suryalohit <51260818+suryalohit@users.noreply.github.com>
2022-01-13 15:38:26 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -13,20 +13,17 @@ api_secret="i3hUtLdolcx5289Atv9oCTWI4ayntw84p8oWOuXDcnXiq9BPklPW9AOjEYqVjVeT"
 client = Client(api_key, api_secret)
 
 # Function that create the app 
-
+def create_app(test_config=None ):
     # create and configure the app
-app = Flask(__name__)
+    app = Flask(__name__)
 
     # Simple route
-@app.route('/')
-def index(): 
-        return jsonify({
-           "status": "success",
-            "message": "Hello World!"
-        }) 
+    @app.route('/')
+    def index(): 
+        return "<h1>Welcome to Geeks for Geeks</h1>"
      
-@app.route('/webhook', methods=['POST'])
-def webhook():
+    @app.route('/webhook', methods=['POST'])
+    def webhook():
 
      data = json.loads(request.data)
 
@@ -138,6 +135,10 @@ def webhook():
          
         
           }) 
-    
- # do not forget to return the app
+    return app # do not forget to return the app
 
+APP = create_app()
+
+if __name__ == '__main__':
+    # APP.run(host='0.0.0.0', port=5000, debug=True)
+    APP.run(debug=True)
