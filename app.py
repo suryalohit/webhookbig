d78a211a24c9065c7f2ab70b45b8618f6895b12d
suryalohit <51260818+suryalohit@users.noreply.github.com>
2022-01-13 14:21:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,148 @@
+from flask import (
+    Flask, 
+    jsonify
+)
+
+import json
+import math
+from flask import Flask, request
+import pandas as pd
+from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
+api_key="cLn6BbpRC8wpE6NIq0Z7ebLkpZez0o8I1X5dEG7EjMvULZPoAlyTc8EmQpaSzv17"
+api_secret="i3hUtLdolcx5289Atv9oCTWI4ayntw84p8oWOuXDcnXiq9BPklPW9AOjEYqVjVeT"
+client = Client(api_key, api_secret)
+
+# Function that create the app 
+def create_app(test_config=None ):
+    # create and configure the app
+    app = Flask(__name__)
+
+    # Simple route
+    @app.route('/')
+    def hello_world(): 
+        return jsonify({
+           "status": "success",
+            "message": "Hello World!"
+        }) 
+     
+    @app.route('/webhook', methods=['POST'])
+    def webhook():
+
+     data = json.loads(request.data)
+
+     if data['passphrase'] != "abcdefgh":
+           return jsonify( {
+            "code": "error",
+            "message": "Bad work"
+           }) 
+     else:
+         side = data['side']
+        
+         
+
+         if side=="Y":
+            try:
+                client.futures_create_order(symbol='SANDUSDT', side='SELL', type='MARKET', quantity=100, reduceOnly='true')
+            
+            except Exception as e:
+                print(type(e).__name__, str(e))
+
+
+         if side=="LONG":
+            try:
+                client.futures_cancel_all_open_orders(symbol='SANDUSDT')
+                print("t1")
+            
+            except Exception as e:
+                print(type(e).__name__, str(e))
+                print("e1")
+            try:
+                cp=client.futures_create_order(symbol='SANDUSDT', side='BUY', type='MARKET', quantity=100, reduceOnly='true')
+                print(cp)
+                print("t2")
+            except Exception as e:
+                print(type(e).__name__, str(e))
+                print("e2")
+            try:
+                print("t3")
+                df=pd.DataFrame(client.futures_account_balance())
+                usdtprice=df[df['asset']=="USDT"]['balance']
+                bal=usdtprice.iloc[0]
+                bal=float(bal)
+                print(bal)
+
+                sandprice=client.futures_symbol_ticker(symbol='SANDUSDT')
+                sandprice=float(sandprice['price'])
+                print(sandprice)
+
+                qt1=math.floor((bal*2)/sandprice)
+                print(qt1)
+        
+                client.futures_create_order(
+                                symbol='SANDUSDT',
+                                type='MARKET',  # The price at which you wish to buy/sell, float
+                                side='BUY',  # Direction ('BUY' / 'SELL'), string
+                                quantity=qt1  # Number of coins you wish to buy / sell, float
+                )
+            
+            except Exception as e:
+                print("e3")
+                print(type(e).__name__, str(e))    
+            
+
+
+
+         if side=="SHORT":
+            try:
+                client.futures_cancel_all_open_orders(symbol='SANDUSDT')
+            
+            except Exception as e:
+                print(type(e).__name__, str(e))
+            try:
+                 cp1=client.futures_create_order(symbol='SANDUSDT', side='SELL', type='MARKET', quantity=100, reduceOnly='true')
+                 print(cp1)
+            except Exception as e:
+                print(type(e).__name__, str(e))
+            try:
+                df=pd.DataFrame(client.futures_account_balance())
+                usdtprice=df[df['asset']=="USDT"]['balance']
+                bal=usdtprice.iloc[0]
+                bal=float(bal)
+                print(bal)
+
+                sandprice=client.futures_symbol_ticker(symbol='SANDUSDT')
+                sandprice=float(sandprice['price'])
+                print(sandprice)
+
+                qt2=math.floor((bal*2)/sandprice)
+                print(qt2)
+                
+                client.futures_create_order(
+                                symbol='SANDUSDT',
+                                type='MARKET',  # The price at which you wish to buy/sell, float
+                                side='SELL',  # Direction ('BUY' / 'SELL'), string
+                                quantity=qt2  # Number of coins you wish to buy / sell, float
+                )
+            
+            except Exception as e:
+                print(type(e).__name__, str(e))   
+            
+            
+       
+        
+         return jsonify( {
+         "code": "error",
+         "message": "good work",
+         "sandprice":sandprice,
+         "usdtbalance":bal
+        
+         
+        
+          }) 
+    return app # do not forget to return the app
+
+APP = create_app()
+
+if __name__ == '__main__':
+    # APP.run(host='0.0.0.0', port=5000, debug=True)
+    APP.run(debug=True)
