cba7574e8739169302d624d248dcf83e99c9540a
suryalohit <51260818+suryalohit@users.noreply.github.com>
2022-09-29 20:36:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ app = Flask(__name__)
     # Simple route
 @app.route('/')
 def index(): 
-        return "<h1>test2</h1>"
+        return "<h1>test3</h1>"
 
 
 @app.route('/telweb', methods=['GET','POST'])
